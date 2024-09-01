2a27e50cfdc6310c75c3c33ac961a345d7ac72f4
833M0L3 <bimaldhital@gmail.com>
2024-09-01 20:52:45 +0545

    test

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "nettvdocs",
   description: "Docs and comprehensive guides about Worldlink Communication's NETTV",
+  base: '/NETTV/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -41,7 +42,6 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/833M0L3/NETTV' }
     ],
-    base: '/NETTV/',
     editLink: {
       pattern: 'https://github.com/833M0L3/NETTV/edit/main/docs/:path'
     }
